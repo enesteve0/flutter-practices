76e188b95a63273e34a419c23ef58d7515cf5ed6
EneSteve <104717844+enesteve0@users.noreply.github.com>
2024-01-30 01:17:59 +0300

    Add files via upload

---
+++ 1 - January 2024/01-29-2024/android/app/src/main/kotlin/com/example/jan29/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jan29
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 1 - January 2024/01-29-2024/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<country_codes/CountryCodesPlugin.h>)
+#import <country_codes/CountryCodesPlugin.h>
+#else
+@import country_codes;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CountryCodesPlugin registerWithRegistrar:[registry registrarForPlugin:@"CountryCodesPlugin"]];
+}
+
+@end
