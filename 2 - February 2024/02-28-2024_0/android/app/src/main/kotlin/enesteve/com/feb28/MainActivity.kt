c69c7aa7b65f1c04ecc180913c1476a94a75bf68
EneSteve <104717844+enesteve0@users.noreply.github.com>
2024-02-28 15:56:24 +0300

    Add files via upload

---
+++ 2 - February 2024/02-28-2024_0/android/app/src/main/kotlin/enesteve/com/feb28/MainActivity.kt
@@ -0,0 +1,5 @@
+package enesteve.com.feb28
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 2 - February 2024/02-28-2024_0/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text(
+          'I Am Rich',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
